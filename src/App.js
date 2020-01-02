8356eef6c43e9b9c63830d43cdeaae7270c17e36
pavanmangena <atulkumar.verma@ltts.com>
2020-01-02 18:24:54 +0530

    implemented simple api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component   {
+  constructor(props)
+  {
+    super(props);
+    this.state={
+      countries:[]
+    }
+  }
+  componentDidMount()
+  {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(result=>result.json())
+    .then(result=>
+      this.setState({  countries:result   }))
+  }
+  render()   {
+     return   (
+      <div>
+        <div>
+          {this.state.countries.map((keys) => (
+            <div>
+              <div>
+                <h3>{keys.name}</h3>
+                <h3>{keys.email}</h3>
+                <h3>{keys.username}</h3>
+                <br/>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+     );
+  }
 }
-
 export default App;
